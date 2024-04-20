9887b4436c7314789ae31efeecd8fe8ad6735cef
EL-Amrani Youssef <elamraniy292@gmail.com>
2024-04-20 15:21:33 +0100

    add: create new user using post

--- src/index.ts
+++ src/index.ts
@@ -30,6 +30,22 @@ app.get("/users/:id",
 );
 
 
+app.post('/users', async (req, res) => {
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    try{
+        const newUser = await user.save();
+        res.json(newUser);
+    }catch(err){
+        res.status(500).json({message: 'user not created'})
+    }
+}
+);
+
 
 
 const port = process.env.PORT || 3000
