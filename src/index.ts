2e5e954b4e436725bd469acfd14a2198cb453456
EL-Amrani Youssef <elamraniy292@gmail.com>
2024-04-20 14:37:39 +0100

    init env for createweb server

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+import express from "express"
+
+const app = express()
+
+
+
+const port = process.env.PORT || 3000
+const host = process.env.HOST
+
+app.listen(port, () => {
+    console.log(`Server is running on ${host}:${port}`)
+})
